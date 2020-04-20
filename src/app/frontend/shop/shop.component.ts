d049d1a3766e722db689061268f48554e7f173d2
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-04-21 04:24:44 +0700

    3 files modified

--- src/app/frontend/shop/shop.component.ts
+++ src/app/frontend/shop/shop.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { ProductService } from '../../product.service';
+import { Product } from '../../Product';
 
 @Component({
   selector: 'app-shop',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+constructor(
+    private productService: ProductService
+  ) { 
+    console.log('constructor')
   }
-
+   selected: Product;
+  products: Product[];
+  ngOnInit(): void {
+    this.getProducts();
+  }
+ 
+  getProducts(){
+   this.productService.getProducts().subscribe(data => {
+     console.log(data);
+     this.products = data;
+    });
+  }
 }
