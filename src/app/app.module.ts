449043e4393efa3553dba03693ee1e9377f2a5ad
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-04-21 01:05:45 +0700

    2 files modified, 20 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
